8e3a6cf60d3fdb92fab290b3d1a515b887c583aa
Sasa Cajipe <sasacajipe@gmail.com>
2020-10-20 00:40:17 +0800

    initial Commit

---
+++ Inventory/Inventory.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Inventory
+{
+    partial class frmAddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtSellPrice = new System.Windows.Forms.TextBox();
+            this.dtPickerMfDate = new System.Windows.Forms.DateTimePicker();
+            this.dtPickerExpDate = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.richTxtDescription = new System.Windows.Forms.RichTextBox();
+            this.gridViewProductList = new System.Windows.Forms.DataGridView();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewProductList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Product";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mfg. Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Exp. Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Qty.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Sell Price";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(58, 50);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(200, 20);
+            this.txtProductName.TabIndex = 7;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(58, 164);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(200, 20);
+            this.txtQuantity.TabIndex = 11;
+            // 
+            // txtSellPrice
+            // 
+            this.txtSellPrice.Location = new System.Drawing.Point(58, 189);
+            this.txtSellPrice.Name = "txtSellPrice";
+            this.txtSellPrice.Size = new System.Drawing.Size(200, 20);
+            this.txtSellPrice.TabIndex = 12;
+            // 
+            // dtPickerMfDate
+            // 
+            this.dtPickerMfDate.Location = new System.Drawing.Point(58, 104);
+            this.dtPickerMfDate.Name = "dtPickerMfDate";
+            this.dtPickerMfDate.Size = new System.Drawing.Size(200, 20);
+            this.dtPickerMfDate.TabIndex = 13;
+            // 
+            // dtPickerExpDate
+            // 
+            this.dtPickerExpDate.Location = new System.Drawing.Point(58, 135);
+            this.dtPickerExpDate.Name = "dtPickerExpDate";
+            this.dtPickerExpDate.Size = new System.Drawing.Size(200, 20);
+            this.dtPickerExpDate.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(280, 53);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Description";
+            // 
+            // richTxtDescription
+            // 
+            this.richTxtDescription.Location = new System.Drawing.Point(287, 77);
+            this.richTxtDescription.Name = "richTxtDescription";
+            this.richTxtDescription.Size = new System.Drawing.Size(216, 109);
+            this.richTxtDescription.TabIndex = 16;
+            this.richTxtDescription.Text = "";
+            // 
+            // gridViewProductList
+            // 
+            this.gridViewProductList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewProductList.Location = new System.Drawing.Point(18, 225);
+            this.gridViewProductList.Name = "gridViewProductList";
+            this.gridViewProductList.Size = new System.Drawing.Size(485, 179);
+            this.gridViewProductList.TabIndex = 17;
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Location = new System.Drawing.Point(419, 192);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(84, 21);
+            this.btnAddProduct.TabIndex = 18;
+            this.btnAddProduct.Text = "Add Product";
+            this.btnAddProduct.UseVisualStyleBackColor = true;
+            this.btnAddProduct.Click += new System.EventHandler(this.btnAddProduct_Click);
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(58, 78);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(200, 21);
+            this.cbCategory.TabIndex = 19;
+            // 
+            // frmAddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 442);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.btnAddProduct);
+            this.Controls.Add(this.gridViewProductList);
+            this.Controls.Add(this.richTxtDescription);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dtPickerExpDate);
+            this.Controls.Add(this.dtPickerMfDate);
+            this.Controls.Add(this.txtSellPrice);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAddProduct";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmAddProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewProductList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtSellPrice;
+        private System.Windows.Forms.DateTimePicker dtPickerMfDate;
+        private System.Windows.Forms.DateTimePicker dtPickerExpDate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox richTxtDescription;
+        private System.Windows.Forms.DataGridView gridViewProductList;
+        private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.ComboBox cbCategory;
+    }
+}
+
---
+++ Inventory/Inventory.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Linq.Expressions;
+
+namespace Inventory
+{
+    public partial class frmAddProduct : Form
+    {
+        private string _ProductName;
+        private string _Category;
+        private string _MfgDate;
+        private string _ExpDate;
+        private string _Description;
+        private int _Quantity;
+        private double _SellPrice;
+        BindingSource showProductList;
+
+
+        public frmAddProduct()
+        {
+            showProductList = new BindingSource();
+            InitializeComponent();
+        }
+
+        private void frmAddProduct_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProductCategory = new string[] {
+               "Beverages",
+               "Bread/Bakey",
+               "Canned/Jarred Goods",
+               "Dairy",
+               "Frozen Goods",
+               "Meat",
+               "Personal Care",
+               "Other",
+            };
+            foreach (string item in ListOfProductCategory)
+            {
+                cbCategory.Items.Add(item);
+            }
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            _ProductName = Product_Name(txtProductName.Text);
+            _Category = cbCategory.Text;
+            _MfgDate = dtPickerMfDate.Value.ToString("yyyy-MM-dd");
+            _ExpDate = dtPickerExpDate.Value.ToString("yyyy-MM-dd");
+            _Description = richTxtDescription.Text;
+            _Quantity = Quantity(txtQuantity.Text);
+            _SellPrice = SellingPrice(txtSellPrice.Text);
+            showProductList.Add(new Product_Class(_ProductName, _Category, _MfgDate, _ExpDate, _SellPrice, _Quantity, _Description));
+            gridViewProductList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            gridViewProductList.DataSource = showProductList;
+
+        }
+
+    public string Product_Name(string name)
+    {
+        try
+        {
+            if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+            {
+
+            }
+        }
+        catch (FormatException fe)
+        {
+            Console.WriteLine(fe.Message);
+        }
+        return name;
+    }
+    public int Quantity(string qty)
+    {
+        try
+        {
+            if (!Regex.IsMatch(qty, @"^[0-9]"))
+            {
+
+            }
+        }
+        catch (NumberFormatException fe)
+        {
+            Console.WriteLine(fe.Message);
+        }
+        return Convert.ToInt32(qty);
+    }
+    public double SellingPrice(string price)
+    {
+        try
+        {
+
+            if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
+            {
+
+            }
+        }
+        catch (CurrencyFormatException fe)
+        {
+            Console.WriteLine(fe.Message);
+        }
+        return Convert.ToDouble(price);
+    }
+}
+    class StringFormatException : Exception
+{
+    public StringFormatException(String name) : base(String.Format("Invalid Input", name))
+    {
+
+    }
+}
+    class NumberFormatException : Exception
+{
+    public NumberFormatException(int num) : base(String.Format("Invalid Input", num))
+    {
+
+    }
+}
+    class CurrencyFormatException : Exception
+{
+    public CurrencyFormatException(int num1) : base(String.Format("Invalid Input", num1))
+    {
+
+    }
+}}
+
+
+
+
+
+       
